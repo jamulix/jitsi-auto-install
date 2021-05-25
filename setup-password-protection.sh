692f9c3e1efe46d467eda3a021a9f6d4796f7e9e
racknerd <84762078+racknerd@users.noreply.github.com>
2021-05-25 18:21:50 +1200

    Update setup-password-protection.sh

--- setup-password-protection.sh
+++ setup-password-protection.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-read "In this next step you need to change the authentication from anonymous to internal_hashed"
-read -p "Hit enter to open the config file then once finish editing save the file by pressing Control + O"
+read -p "In this next step you need to change the authentication from anonymous to internal_hashed.  Hit enter to open the config file then once finish editing save the file by pressing Control + O"
 nano /etc/prosody/conf.d/$(hostname -f).cfg.lua
 read -p "Next step is to add the following to the end of the configuration file and then save"
-read "VirtualHost "guest.meet.racknerddemo.com" "
-read "   authentication = "anonymous" "
-read "   c2s_require_encryption = false "
+read -p "VirtualHost "guest.meet.racknerddemo.com" "
+      "   authentication = "anonymous" "
+      "   c2s_require_encryption = false "
 read -p "copy above then hit enter to open configuration file"
 nano /etc/prosody/conf.d/$(hostname -f).cfg.lua
